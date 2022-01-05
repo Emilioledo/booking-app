7de6244f71f6668ff941c09b1d8d56e13ff983b8
Emilio Fernandez Ledo <EF_E91097@redlink.com.ar>
2022-01-05 16:15:06 -0300

    printf

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main () {
-	fmt.Println("Welcome to our conference booking app")
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking app\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
