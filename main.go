9272858a998f31d326e5436ffe51a0a675e7015d
Emilio <emilio@pop-os.localdomain>
2022-01-10 16:58:35 -0300

    go.mod

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func main () {
-	var conferenceName = "Go conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+func main() {
+	var conferenceName string = "Go conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
 
-	fmt.Printf("Welcome to %v booking app\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Printf("Welcome to %v booking app \n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
